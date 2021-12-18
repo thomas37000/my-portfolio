7cbeef5af8f85ef5d922e58c5f5f04ffd63f46c4
thomas <thomas.chalanson@sfr.fr>
2021-12-18 16:46:50 +0100

    install react bootstrap

---
+++ src/components/navbar/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import logo from "../navbar/frontend.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} class="logo" alt="my-logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="#ancre-présentation">Présentation</Nav.Link>
+              <Nav.Link href="#ancre-skills">Skills</Nav.Link>
+              <Nav.Link href="#ancre-projes">Projets</Nav.Link>
+              <Nav.Link href="#ancre-contact">Contact</Nav.Link>
+              <Nav.Link eventKey={2} href="#ancre-cv">
+                Cv
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
