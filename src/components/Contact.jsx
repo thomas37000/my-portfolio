611fcee175f7061d105536b39320bf592176b171
thomas <thomas.chalanson@sfr.fr>
2021-12-26 18:34:49 +0100

    style the footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import '../App.css';
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       Des questions, envoyez moi un email à : thomas.chalanson@gmail.com - tel :
       0663802804
     </div>
