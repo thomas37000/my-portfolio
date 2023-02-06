83fbb611cbbfc554f69d9d3ba1144ed98e58d6ab
thomas37000 <thomas.chalanson@sfr.fr>
2023-02-06 19:13:41 +0100

    add new json perso project

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,32 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import Cv from "../../cv/Thomas Chalanson Cv.pdf";
-import Networks from "../Networks";
-import "./Contact.css";
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Cv from '../../cv/thomas-chalanson-cv.pdf';
+import CvAlternance from '../../cv/thomas-chalanson-cv-alternance.pdf';
+import Networks from '../Networks';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <>
-      <footer className="contact-container">
-        <div className="cv">
-          <Button variant="light" className="btnCv">
-            <a className="cv-btn" href={Cv} download>
+      <footer className='contact-container'>
+        <div className='cv'>
+          <Button variant='light' className='btnCv'>
+            <a className='cv-btn' href={Cv} download>
               téléchargez mon Cv
             </a>
           </Button>
         </div>
-        <div className="contact">
-          Des questions, envoyez moi un email à thomas.chalanson@gmail.com -
-          tel : 0663802804
+
+        <div className='cv'>
+          <Button variant='light' className='btnCv'>
+            <a className='cv-btn' href={CvAlternance} download>
+              téléchargez mon Cv pour l' Alternance
+            </a>
+          </Button>
+        </div>
+        <div className='contact'>
+          Des questions, envoyez moi un email à thomas.chalanson@gmail.com - tel
+          : 0663802804
         </div>
 
         <Networks />
